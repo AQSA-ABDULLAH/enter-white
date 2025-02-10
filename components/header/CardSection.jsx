0bdf257479e9048bc0918a4ea790d2121bc367f2
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2025-02-10 15:19:57 +0500

    fix styling

--- components/header/CardSection.jsx
+++ components/header/CardSection.jsx
@@ -3,14 +3,14 @@ import { FaPlus, FaMinus } from "react-icons/fa6";
 
 const Card = ({ title, subtitle, description, buttonText, showControls }) => (
   <div className="flex text-white">
-    <div className="relative w-[180px] 2xl:w-[212px] flex flex-col items-center bg-cover bg-center tracking-widest">
-      <div className="bg-black bg-opacity-40 w-full h-[160px] 2xl:h-[212px] p-4 border border-white rounded-lg text-center flex flex-col justify-between">
-        <h2 className="text-[15px] 3xl:text-[20px]">{title}</h2>
-        <p className="text-[15px] 3xl:text-[20px]">{subtitle}</p>
-        <p className="text-[7px] 3xl:text-[12px]">{description}</p>
+    <div className="relative w-[140px] sm:w-[180px] 2xl:w-[212px] flex flex-col items-center bg-cover bg-center tracking-widest">
+      <div className="bg-black bg-opacity-40 w-full h-[140px] sm:h-[160px] 2xl:h-[212px] p-4 border border-white rounded-lg text-center flex flex-col justify-between">
+        <h2 className="text-[12px] sm:text-[15px] 3xl:text-[20px]">{title}</h2>
+        <p className="text-[12px] sm:text-[15px] 3xl:text-[20px]">{subtitle}</p>
+        <p className="text-[5px] sm:text-[7px] 3xl:text-[12px]">{description}</p>
       </div>
-      <div className="bg-black bg-opacity-40 h-[38px] 2xl:h-[45px] mt-[10px] flex justify-center rounded-lg border border-white text-center w-full">
-        <button className="hover:bg-white hover:text-black transition text-[14px] 3xl:text-[20px] tracking-widest">
+      <div className="bg-black bg-opacity-40 h-[34px] sm:h-[38px] 2xl:h-[45px] mt-[10px] flex justify-center rounded-lg border border-white text-center w-full">
+        <button className="hover:bg-white hover:text-black transition text-[10px] sm:text-[14px] 3xl:text-[20px] tracking-widest">
           {buttonText}
         </button>
       </div>
@@ -18,7 +18,7 @@ const Card = ({ title, subtitle, description, buttonText, showControls }) => (
     
     {/* Conditional rendering for Plus/Minus controls */}
     {showControls && (
-      <div className="bg-black bg-opacity-40 w-[38px] 2xl:w-[45px] h-[208px] 2xl:h-[262px] ml-4 p-4 flex flex-col justify-between items-center rounded-lg border border-white">
+      <div className="bg-black bg-opacity-40 w-[34px] sm:w-[38px] 2xl:w-[45px] h-[186px] sm:h-[208px] 2xl:h-[262px] ml-4 p-4 flex flex-col justify-between items-center rounded-lg border border-white">
         <div><FaPlus /></div>
         <div><FaMinus /></div>
       </div>
@@ -28,7 +28,7 @@ const Card = ({ title, subtitle, description, buttonText, showControls }) => (
 
 function CardSection() {
   return (
-    <div className="flex flex-col sm:flex-row justify-around gap-12 sm:gap-0 sm:w-[100%]">
+    <div className="flex flex-col sm:flex-row justify-around gap-16 sm:gap-0 sm:w-[100%]">
       <Card
         title="Postal Entry"
         subtitle="No Purchase Necessary"
