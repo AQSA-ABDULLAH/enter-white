c6142e7162d8e04fa505d2daff503a8522931e9c
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2025-02-11 10:03:08 +0500

    fix issues

--- components/header/CardSection.jsx
+++ components/header/CardSection.jsx
@@ -15,15 +15,15 @@ const Card = ({ title, subtitle, description, buttonText, showControls }) => {
 
   return (
     <div className="flex text-white">
-      <div className="relative w-[140px] sm:w-[180px] 2xl:w-[212px] flex flex-col items-center bg-cover bg-center tracking-widest">
-        <div className="bg-black bg-opacity-40 w-full h-[140px] sm:h-[160px] 2xl:h-[212px] p-4 border border-white rounded-lg text-center flex flex-col justify-between">
+      <div className="relative w-[120px] sm:w-[180px] 2xl:w-[212px] flex flex-col items-center bg-cover bg-center tracking-widest">
+        <div className="bg-black bg-opacity-40 w-full h-[120px] sm:h-[160px] 2xl:h-[212px] p-4 border border-white rounded-lg text-center flex flex-col justify-between">
           <h2 className="text-[12px] sm:text-[15px] 3xl:text-[20px]">
             {title === "1 TICKET(S)" ? `${ticketCount} TICKET(S)` : title}
           </h2>
           <p className="text-[12px] sm:text-[15px] 3xl:text-[20px]">£{totalPrice.toFixed(2)}</p>
           <p className="text-[5px] sm:text-[7px] 3xl:text-[12px]">{description}</p>
         </div>
-        <div className="bg-black bg-opacity-40 hover:bg-white hover:text-black h-[34px] sm:h-[38px] 2xl:h-[45px] mt-[10px] flex justify-center rounded-lg border border-white text-center w-full">
+        <div className="bg-black bg-opacity-40 hover:bg-white hover:text-black h-[30px] sm:h-[38px] 2xl:h-[45px] mt-[6px] sm:mt-[10px] flex justify-center rounded-lg border border-white text-center w-full">
           <button className="transition text-[10px] sm:text-[14px] 3xl:text-[20px] tracking-widest">
             {buttonText}
           </button>
@@ -32,7 +32,7 @@ const Card = ({ title, subtitle, description, buttonText, showControls }) => {
 
       {/* Conditional rendering for Plus/Minus controls */}
       {showControls && (
-        <div className="bg-black bg-opacity-40 w-[34px] sm:w-[38px] 2xl:w-[45px] h-[186px] sm:h-[208px] 2xl:h-[262px] ml-4 p-4 flex flex-col justify-between items-center rounded-lg border border-white">
+        <div className="bg-black bg-opacity-40 w-[18px] sm:w-[38px] 2xl:w-[45px] h-[156px] sm:h-[200px] 2xl:h-[262px] ml-4 p-4 flex flex-col justify-between items-center rounded-lg border border-white">
           <button onClick={increaseTicket} className="text-white">
             <FaPlus />
           </button>
